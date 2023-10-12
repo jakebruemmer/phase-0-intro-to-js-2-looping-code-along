ce102470e6b9902f38c4eb4583ddbf5f701d81a2
Jake Bruemmer <jakebruemmer@gmail.com>
2023-10-12 12:12:57 -0600

    Got tests passing.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+let writeCards = function (names) {
+  let thankYouMsgs = [];
+  for (var i = 0; i < names.length; i++) {
+    thankYouMsgs.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+  return thankYouMsgs;
+}
+
+let countDown = function (number) {
+  for (var i = number; i > -1; i--) {
+    console.log(i);
+  }
+}
